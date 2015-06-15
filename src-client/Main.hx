26e2c05094eac176cf9ed884bc2076b62b9900b3
NicoM1 <nico.may99@gmail.com>
2015-06-14 20:29:45 -0700

    forgot I didn't need to escape those

--- src-client/Main.hx
+++ src-client/Main.hx
@@ -825,7 +825,6 @@ class Main
 		parsed = parsed.htmlEscape();
 		parsed = parsed.replace('\"', '&quot;');
 		parsed = parsed.replace(':', '&colon;');
-		//parsed = parsed.replace('\\\\', '&bsol;');
 		parsed = parsed.replace('\\*', '&ast;');
 		parsed = parsed.replace('\\#', '&num;');
 		
