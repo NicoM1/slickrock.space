61405b2739b747c073bc688743db65b72c3d5933
NicoM1 <nico.may99@gmail.com>
2015-06-27 00:12:48 -0700

    don't use actual objectID, use hex

--- src/Main.hx
+++ src/Main.hx
@@ -501,19 +501,19 @@ class RouteHandler implements abe.IRoute {
 		response.send('failed');
 	}
 
-	//@:post('/api/deleteMessage/:room/:privatePass/:id')
-	//function deleteMessage(room: String, privatePass: String, id: ObjectID) {
-		/*room = room.toLowerCase();
+	@:post('/api/deleteMessage/:room/:privatePass/:id')
+	function deleteMessage(room: String, privatePass: String, id: String) {
+		room = room.toLowerCase();
 		var roomE = Main.rooms.get(room);
 		if (roomE.pw == Sha1.encode(roomE.salt + privatePass)) {
-			Main.deleteMessage(room, id);
+			Main.deleteMessage(room, ObjectID.createFromHexString(id));
 			response.setHeader('Access-Control-Allow-Origin', '*');
 			response.send('deleted');
 			return;
 		}
 		response.setHeader('Access-Control-Allow-Origin', '*');
-		response.send('failed');*/
-	//}
+		response.send('failed');
+	}
 
 	@:post('/api/unlock/:room/:privateID/:privatePass')
 	function unlockRoom(room: String, privateID: String, privatePass: String) {
