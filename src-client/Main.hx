c2128fbdf86d5f55ad3407ed1c639ab977d8de2b
NicoM1 <nico.may99@gmail.com>
2015-06-30 19:49:09 -0700

    Revert "scroll a different way"
    This reverts commit 8c5a22b73d28e30458fce5ed6a7ba2ca0e5db8d7.

--- src-client/Main.hx
+++ src-client/Main.hx
@@ -1021,7 +1021,7 @@ class Main
 
 	function _tryScroll(force: Bool = false, img: ImageElement = null) {
 		if (force || _atBottom(img)) {
-			messages.scrollTop = messages.scrollHeight;
+			messages.scrollTo(0, messages.scrollHeight);
 			initialScroll = false;
 		}
 	}
