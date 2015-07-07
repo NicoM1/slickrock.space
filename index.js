667e0a32b4c83c543088b558d73ffa39d3691205
NicoM1 <nico.may99@gmail.com>
2015-07-07 00:07:16 -0700

    remove bridge

--- index.js
+++ index.js
@@ -499,7 +499,8 @@ Main.addMessage = function(message,id,room,irc) {
 	var objectid = new js_node_mongodb_ObjectID();
 	Main.rooms.get(room).messages.push({ text : message, id : id, _id : objectid.toHexString()});
 	Main.saveMessage({ text : message, id : id, room : room, _id : objectid});
-	if(!irc) Main.ircClient.say("#" + room,"<" + id + "> " + message);
+	if(!irc) {
+	}
 };
 Main.hasMongo = function() {
 	return Main.mongodb != null;
